b4b29b4971ffa5c3be0673b3b576147ec7b8a0ae
HpProBook\Admin <a-panin@mail.ru>
2018-06-28 14:34:33 +0300

    Дверь и триггер

---
+++ Source/TEST_01/OpenDoor.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TEST_01_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:	
+	AActor* Object = nullptr;
+	bool IsOPen = false;
+	FRotator CurrentRorartor;
+	UPROPERTY(EditAnywhere)
+		int OpenDoorAngle;
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* OpenDoorTrigger;
+
+	void OpenDoor();
+	void CloseDoor();
+
+	
+};
